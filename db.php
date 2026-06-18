e6a71a14805355c00d3642ab4e3359cdc2cb2a92
cartooncity578-create <cartooncity578@gmail.com>
2026-06-18 15:28:27 +0600

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host = "localhost";
-$user = "DB_USERNAME"; // Yahan apne database ka username daalo
-$pass = "DB_PASSWORD"; // Yahan password daalo
-$dbname = "zubaygames"; // Database ka naam
-
-$conn = mysqli_connect($host, $user, $pass, $dbname);
-if (!$conn) { die("Connection failed: " . mysqli_connect_error()); }
-?>
