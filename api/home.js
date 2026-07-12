0df5953c9bfa674ab3ae5cfece8a3e0ea3369d81
cartooncity578-create <cartooncity578@gmail.com>
2026-07-12 19:05:12 +0600

    Update home.js

--- api/home.js
+++ api/home.js
@@ -1,23 +1,7 @@
 export default function handler(req, res) {
-  // Yahan tumhara data structure hoga
-  let data = {
+  res.status(200).json({
     catalog: {
-      "item1": { name: "Minecraft", image: "...", redirect: "minecraft.html" }
+      "item1": { name: "Minecraft", image: "https://placehold.co/100", redirect: "minecraft.html" }
     }
-  };
-
-  if (req.method === 'POST') {
-    const { type, key, name, image, redirect, newName, newImg, newLink } = req.body;
-
-    if (type === 'add') {
-      data.catalog['item' + Date.now()] = { name, image, redirect };
-    } else if (type === 'update') {
-      data.catalog[key] = { name: newName, image: newImg, redirect: newLink };
-    } else if (type === 'delete') {
-      delete data.catalog[key];
-    }
-    return res.status(200).json({ success: true });
-  }
-
-  res.status(200).json(data);
+  });
 }
